acee8342a77f365d07c75ac71e1f387d727b6dd4
Julia Azevedo <jhuliabeatriz@hotmail.com>
2018-05-29 21:25:05 -0300

    icones na navbar

--- application/views/frontend/template/header.php
+++ application/views/frontend/template/header.php
@@ -4,49 +4,67 @@
 
         <div class='preloader'><div class='loaded'>&nbsp;</div></div>
 
-        <nav class="navbar navbar-fixed-top navbar-light bg-faded"  style="background-color: rgba(0,0,0,0);     overflow-y: hidden; ">
+        <nav class="navbar navbar-fixed-top  bg-faded"  style="  background-color: rgba(0,0,0,0); height: 90px; padding-bottom: 10px; color: #ebe9e9;">
             <!--Collapse button-->
             <div class="container">
-                <a href="#" data-activates="mobile-menu" class="button-collapse right"><i class="fa fa-bars black-text"></i></a>
-
+                <a href="#" data-activates="mobile-menu" class="button-collapse right"><i class="fa fa-bars "></i></a>
+                
                 <!--Content for large and medium screens-->
-                <div class="navbar-desktop" style="margin-top:-10px">
+                <center>
+                <div class="navbar-desktop " style="margin-top:-10px">
                     <!--Navbar Brand-->
-                    <a class="navbar-brand" href="#home" ><img src="/iceb/assets/frontend/img/logo-nome.png" alt="" style="width: 150px;  " /></a>
+                    <a class="navbar-brand" href="#home" ><img scr="/iceb/assets/frontend/img/ufop.png" alt="" style="width: 15px;"></a>
                     <!--Links-->
-                    <ul class="nav navbar-nav pull-right hidden-md-down text-uppercase" style="margin-top:10px;  ">
-                        <li class="nav-item">
+                    <ul class="nav navbar-nav  text-uppercase" style="margin-top:20px;  ">
 
-                            <a class="nav-link" href="#home"><span class="fa fa-home "></span> Inicio <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#about" style="color:#ed954e;"> <span class="fa fa-university "> </span>O Instituto</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#service" style="color:#ed954e;"><span class="fa fa-briefcase"></span>Departamentos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#team" style="color:#ed954e;"><span class="fa fa-flask"></span>Laboratórios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#team" style="color:#ed954e;"><span class="fa fa-binoculars"></span>Pesquisa</a>
-                        </li>
-                        <!--
-                        <li class="nav-item">
-                            <a class="nav-link" href="#portfolio"><span class="fa fa-calendar"></span>Agenda ICEB</a>
-                        </li>
-                        -->
-                        <li class="nav-item">
-                            <a class="nav-link" href="#newsletter" style="color:#ed954e;"><span class="fa fa-newspaper-o"> </span> Noticias</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" target="pesquisa" href="#!" style="color:#ed954e;"><i class="fa fa-search fa-lg"></i></a>
-                        </li>
-                        
+                        <center><li class="nav-item" style="margin: 0px 15px 0px 15px ">
+                            <img src="/iceb/assets/frontend/img/icones/home.png" style="width: 30px; padding-bottom: 15px">
+                            <a class="nav-link" href="#home" style="margin-top: -45px; ">  Inicio <span class="sr-only">(current)</span></a>
+                        </li></center>
+
+                         <center><li class="nav-item" style="margin: 0px 15px 0px 15px ">
+                            <img src="/iceb/assets/frontend/img/icones/instituto.png" style="width: 30px; padding-bottom: 15px">
+                             <a class="nav-link" href="#about" style="margin-top: -45px;  "> O Instituto</a>
+                        </li></center>
+
+                        <center><li class="nav-item" style="margin: 0px 15px 0px 15px ">
+                            <img src="/iceb/assets/frontend/img/icones/departamento.png" style="width: 25px; padding-bottom: 15px">
+                            <a class="nav-link" href="#service" style="margin-top: -45px;" >Departamentos</a>
+                        </li></center>
+
+                        <center><li class="nav-item" style="margin: 0px 15px 0px 15px ">
+                            <img src="/iceb/assets/frontend/img/icones/ensino.png" style="width: 30px;padding-bottom: 15px">
+                             <a class="nav-link" href="#team" style="margin-top: -45px;  ">Ensino</a>
+                        </li></center>
+                        <center><li class="nav-item" style="margin: 0px 15px 0px 15px ">
+                            <img src="/iceb/assets/frontend/img/icones/laboratorio.png" style="width: 30px;padding-bottom: 15px">
+                             <a class="nav-link" href="#team" style="margin-top: -45px;  ">Laboratórios</a>
+                        </li></center>
+
+                        <center><li class="nav-item" style="margin: 0px 15px 0px 15px ">
+                            <img src="/iceb/assets/frontend/img/icones/agenda.png" style="width: 30px; padding-bottom: 15px">
+                            <a class="nav-link" href="#team" style="margin-top: -45px;  ">Agenda </a>
+                        </li></center>
+                        <center><li class="nav-item" style="margin: 0px 15px 0px 15px ">
+                            <img src="/iceb/assets/frontend/img/icones/pesquisa.png" style="width: 35px; padding-bottom: 15px">
+                            <a class="nav-link" href="#team" style="margin-top: -45px;  ">Pesquisa</a>
+                        </li></center>
+
+                        <center><li class="nav-item" style="margin: 0px 15px 0px 15px ">
+                            <img src="/iceb/assets/frontend/img/icones/news.png" style="width: 35px;padding-bottom: 15px">
+                            <a class="nav-link" href="#team" style="margin-top: -45px;  ">Noticias</a>
+
+                        </li></center>
+
+                        <center><li class="nav-item" style="margin: 0px 15px 0px 15px ">
+                            <img src="/iceb/assets/frontend/img/icones/contato.png" style="width: 35px;padding-bottom: 10px">
+                             <a class="nav-link" href="#team" style="margin-top: -45px;  ">Contato</a>
+                        </li></center>
                     </ul>
+
                     
                 </div>
-
+                </center>
                 <!-- Content for mobile devices-->
                 <div class="navbar-mobile">
 
