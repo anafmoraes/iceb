ffececad9f0bff69bc685e70e26c988a0c19637c
Julia Azevedo <jhuliabeatriz@hotmail.com>
2018-07-31 12:33:37 -0300

    iceb centralizado - imagem descentralizada

--- application/views/frontend/homeprincipal.php
+++ application/views/frontend/homeprincipal.php
@@ -1,16 +1,15 @@
-<div id="home" class="slider" >
-            <ul class="slides" >
+<div id="home" >
 
-                <li >
-                    <img src="<?php echo base_url('assets/frontend/img/ICEBERA.png')?>" style="position: absolute" > <!-- random image -->
-                    <div class="caption center-align">
-                        <div class="single_home" >
-                            <img src="<?php echo base_url('assets/frontend/img/logoiceb.png') ?>" style="width: 250px;  height: 145px; margin-left:auto; margin-right: auto; margin-bottom: 50px "  >
+
+
+                    <div style=" width:100%; height: auto; background: url('assets/frontend/img/ICEBERA.png'); padding: 225px;" > 
+
+                    <div class="center-align">
+
+                            <img src="<?php echo base_url('assets/frontend/img/logoiceb.png') ?>" style="width: 250px;  height: 145px;  margin-bottom: 50px "  >
                             <br>
-                            <h2 style="font-size: 125px;">ICEB</h2>
-                        </div>
+                            <h2 style="font-size: 125px; color: white;">ICEB</h2>
+     
                     </div>
-                </li>
-
-            </ul>
+                    </div>
         </div>
