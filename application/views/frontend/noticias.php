e25996a7546983ea1ab696bba8f528ad67de11a3
Ana Moraes <ana96moraes@gmail.com>
2018-08-19 23:46:55 -0300

    Adição de Destaque na página inicial

--- application/views/frontend/noticias.php
+++ application/views/frontend/noticias.php
@@ -5,43 +5,27 @@
                 <div class="row">
                     <div class="main_portfolio_area m-y-3">
                         <div class="head_title center wow ">
-                            <h2>Noticias</h2>
-
+                            <h2>Destaques</h2>
                         </div>
-                            <?php foreach($noticias as $noticia) {?>
-                            <div id="mixcontent" class="mixcontent  wow">
-                                <div class="col-md-4 mix cat4 cat2">
-                                    <div class="single_mixi_portfolio center">
-                                        <div class="single_portfolio_img">
-                                            <img src="<?php echo base_url('assets/frontend/img/noticiasimg/sfoto.jpg')?>" alt="" />
-                                            <div class="mixi_portfolio_overlay">
-                                                <a href="#!"><i class="fa fa-search"></i></a>
-                                                <a href="<?php echo base_url('assets/frontend/noticiasimg/sfoto.jpg') ?>" class="gallery-img"><i class="fa fa-link"></i></a>
-                                            </div>
-                                        </div>
-                                        <div class="single_portfolio_text">
-                                            <h3><?php echo $noticia->titulo ?></h3>
-                                            <?php echo postadoem($noticia->data) ?>
-                                        </div>
-                                    </div>
-                                </div>
-                              <?php } ?>
-                                <br>
-                            </hr>
-                            <br>
-
-
-                            </div>
-
+                        <div>
+                            <center><h3>ICEB - recepção de calouros</h3>
+                            <img src="<?php echo base_url('assets/frontend/img/noticiasimg/SemanadeRecepcao.jpeg') ?>" style="width: 450px"></center>
+                            <div class="service_btn center"><a target="_blank" href="https://www.ufop.br/eventos/iceb-recepcao-de-calouros" class="btn waves-effect waves-red">&nbsp;  Saiba mais&nbsp;  </a></div>
+                        </div>
+                        <br>
+                        <div>
+                            <center><h3>Profa Jaqueline, do ICEB, é destaque em premiação nacional</h3>
+                            <img src="<?php echo base_url('assets/frontend/img/noticiasimg/premiacaoJaqueline.jpg') ?>" style="width: 450px"></center>
+                            <div class="service_btn center"><a target="_blank" href="https://www.em.com.br/app/noticia/gerais/2018/08/14/interna_gerais,980025/pesquisadoras-da-ufmg-e-da-ufop-conquistam-premiacoes.shtml" class="btn waves-effect waves-red">&nbsp;  Saiba mais&nbsp;  </a></div>
+                        </div>
+                        
                         </div>
                     </div>
                 </div>
             </div>
 
             <br>
-             <div class="service_btn center">
-                 <a href="<?php echo base_url('noticias'); ?>" class="btn waves-effect waves-red">&nbsp;  VER MAIS&nbsp;  </a>
-            </div>
+             
 
         </section><!-- End of Portfolio Section -->
           </div>
